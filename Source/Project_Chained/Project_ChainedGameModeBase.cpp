c6f435a350603a7ac26512c341d43375e0b4bebe
Jago1996 <jezr96@hotmail.com>
2017-01-31 19:42:40 -0600

    Initial commit

---
+++ Source/Project_Chained/Project_ChainedGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project_Chained.h"
+#include "Project_ChainedGameModeBase.h"
+
+
+
+
---
+++ Source/Project_Chained/Project_ChainedGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Project_ChainedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_CHAINED_API AProject_ChainedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
